1c7b3a6ac52cd4c6e37cd6c39fef760912885898
laurent.bardiaux <laurent.bardiaux@gmail.com>
2024-09-08 14:04:02 +0000

    add comment and generate README with cargo readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# srvstat
+
+## Metrics Publisher
+
+A lightweight Rust application that reads system metrics (such as Disk, CPU, and Memory usage) from the host and publishes them to an MQTT queue.
+
+### Features
+
+- Reads metrics from the system.
+- Publishes metrics to an MQTT queue.
+- Supports disk, CPU, and memory metrics.
+
+### Project Structure
+
+- `config`: Contains configuration settings for the application.
+- `domain`: Defines core domain logic, such as metric categories and services.
+- `outbound`: Handles outbound operations, including reading and writing metrics.
+
+### Usage
+
+1. Clone the repository:
+    ```bash
+    git clone <repository-url>
+    ```
+
+2. Build the project:
+    ```bash
+    cargo build --release
+    ```
+
+3. Set environment variables for configuration (such as server port):
+    ```bash
+    export SERVER_PORT=8080
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+//! # Metrics Publisher
+//! 
+//! A lightweight Rust application that reads system metrics (such as Disk, CPU, and Memory usage) from the host and publishes them to an MQTT queue.
+//! 
+//! ## Features
+//! 
+//! - Reads metrics from the system.
+//! - Publishes metrics to an MQTT queue.
+//! - Supports disk, CPU, and memory metrics.
+//! 
+//! ## Project Structure
+//! 
+//! - `config`: Contains configuration settings for the application.
+//! - `domain`: Defines core domain logic, such as metric categories and services.
+//! - `outbound`: Handles outbound operations, including reading and writing metrics.
+//! 
+//! ## Usage
+//! 
+//! 1. Clone the repository:
+//!     ```bash
+//!     git clone <repository-url>
+//!     ```
+//! 
+//! 2. Build the project:
+//!     ```bash
+//!     cargo build --release
+//!     ```
+//! 
+//! 3. Set environment variables for configuration (such as server port):
+//!     ```bash
+//!     export SERVER_PORT=8080
+//!
+
 use config::Config;
 use domain::{metric::Category, metric_service::MetricService, ports::MetricProcessor};
 use outbound::{metric_reader::DummyMetricReader, metric_writer::DummyMetricWriter};
@@ -5,7 +38,6 @@ use outbound::{metric_reader::DummyMetricReader, metric_writer::DummyMetricWrite
 pub mod config;
 pub mod domain;
 pub mod outbound;
-
 
 fn main() {
     let config = Config::from_env().expect("Error reading parameters.");
