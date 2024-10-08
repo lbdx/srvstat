9f8e234afc9a298943e8cf7fabcbdf39002386ed
laurent.bardiaux <laurent.bardiaux@gmail.com>
2024-10-08 18:59:36 +0200

    add docs

---
+++ docs/doc/trait.impl/serde/de/trait.Deserialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"srvstat":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"srvstat/domain/ha/models/struct.HomeAssistantDiscoveryConfig.html\" title=\"struct srvstat::domain::ha::models::HomeAssistantDiscoveryConfig\">HomeAssistantDiscoveryConfig</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
